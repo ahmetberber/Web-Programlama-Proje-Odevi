86faa8419884f304753b9c8df7f4c581998ebaaf
Ahmet Hilmi Berber <ahmethberber@gmail.com>
2024-11-10 23:19:11 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Web_Programlama_Proje_Odevi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
